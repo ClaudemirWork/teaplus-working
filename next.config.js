93aa60649251a6201dee15e213fd706c82e7c200
ClaudemirWork <claudemir_pereira@outlook.com.br>
2025-08-05 14:41:52 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,18 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true' }); module.exports = withBundleAnalyzer({}); 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // Ignora erros de TypeScript durante build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignora erros de ESLint durante build
+    ignoreDuringBuilds: true,
+  },
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
