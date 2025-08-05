3a11088e554b90ae258de8c85524908e8b34123c
ClaudemirWork <claudemir_pereira@outlook.com.br>
2025-08-05 14:53:22 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Ignora erros de TypeScript durante build
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ignora erros de ESLint durante build
     ignoreDuringBuilds: true,
-  },
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
   }
 }
 
