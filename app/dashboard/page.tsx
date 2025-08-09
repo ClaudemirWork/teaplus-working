fff88a18e47d88ddd207fb93559918862c61f7ce
ClaudemirWork <claudemir_pereira@outlook.com.br>
2025-08-08 23:37:47 -0300

    Criando pasta dashboards e page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import DashboardCharts from '@/components/charts/DashboardCharts';
+
+export default function DashboardPage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <DashboardCharts />
+    </div>
+  );
+}
