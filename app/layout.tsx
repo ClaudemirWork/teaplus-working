b0f1f8bd763bd5a26b1d0721e72042504edbc443
ClaudemirWork <claudemir_pereira@outlook.com.br>
2025-08-12 18:47:01 -0300

    Update layout.tsx com adição de eye-tracker para atividades de TDAH

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes" />
+        <script src="https://webgazer.cs.brown.edu/webgazer.js" defer></script>
       </head>
       <body className={`${geistSans.className} antialiased`}>
         <ThemeProvider
