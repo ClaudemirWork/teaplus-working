c550a6d599cb5e390263ea9c1d295c38c4c037d6
ClaudemirWork <claudemir_pereira@outlook.com.br>
2025-09-16 14:16:24 -0300

    Create ResultsScreen.tsx

---
+++ app/bubble-pop/components/ResultsScreen.tsx
@@ -0,0 +1,12 @@
+// app/bubble-pop/components/ResultsScreen.tsx
+'use client';
+
+import React from 'react';
+import styles from '../bubble-pop.module.css';
+import { Equipment } from '@/app/types/bubble-pop'; // Ajuste
+
+// ... (Definição de props) ...
+
+export const ResultsScreen = React.memo(({ /* ...props... */ }: ResultsScreenProps) => {
+    // ... (JSX da tela de resultados) ...
+});
